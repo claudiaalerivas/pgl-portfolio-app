5d18cca048e47e84b74bbc4fcf939eed0809c2c3
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-10-17 21:53:22 +0200

    fix(refactoring): remove unnecessary imports

--- components/PersonalInformation/PersonalInformation.tsx
+++ components/PersonalInformation/PersonalInformation.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Pressable, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 export const PersonalInformation = () => {
   return (
     <View>
