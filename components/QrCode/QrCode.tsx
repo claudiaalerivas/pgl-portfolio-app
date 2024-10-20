ce824d74cda9482fa6484f64739ed8faf5652565
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-10-20 12:22:39 +0200

    feat(QrCode): add personal information

--- components/QrCode/QrCode.tsx
+++ components/QrCode/QrCode.tsx
@@ -5,25 +5,23 @@ import QRCode from 'react-native-qrcode-svg';
 const QrCode = () => {
   return (
     <View style={styles.bodyStyles}>
-      <View style={styles.centerQR}>
-        <QRCode value="https://github.com/adhernea" />
+      <View style={styles.CentrarcodigoQR}>
+        <QRCode value="https://github.com/claudiaalerivas" />
       </View>
     </View>
   )
 }
 
+
 const styles = StyleSheet.create({
   bodyStyles: {
     width: '100%',
-    borderWidth: 2,
-    borderColor: 'black',
     alignItems: 'center',
     justifyContent: 'space-between',
     height: '85%'
   },
-  centerQR: {
+  CentrarcodigoQR: {
     justifyContent: 'center',
-    borderWidth: 1,
     width: '100%',
     height: '100%',
     alignItems: 'center'
