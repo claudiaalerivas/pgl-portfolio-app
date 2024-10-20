a9e76284e2afacc3aec302afadb46d0ce5df5b43
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-10-20 12:19:35 +0200

    feat(HobbiesContainer): hobbiesContainer modification, add component Hobbies

--- components/HobbiesContainer/HobbiesContainer.tsx
+++ components/HobbiesContainer/HobbiesContainer.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Hobbies from '../Hobbies/Hobbies';
 
 const HobbiesContainer = () => {
   return (
-    <View>
+    <View style={styles.containerPrincipal}>
       <Text style={styles.title}>
-        cosas que me gustan mucho:
+        Hobbies
       </Text>
       <View style={styles.container}>
         <Hobbies />
       </View>
     </View >
   )
-};
+}
 const styles = StyleSheet.create({
+  containerPrincipal:{
+    backgroundColor: '#e2e2e2',
+    width:'100%'
+  },
+  title: { 
+    marginTop: '5%' ,
+    fontSize: 30, 
+    color: '#0e76bc',
+  },
   container: {
-    marginBottom: '50%',
-    marginTop: '3%'
-  }, 
-  title: {
-    color: 'black', 
-    fontWeight: "900", 
-    textTransform: 'capitalize', 
-    fontSize: 20, 
-    textAlign: 'center'
+    marginBottom: '125%',
   }
 });
-export default HobbiesContainer;
+export default HobbiesContainer
