76aa07e6244ac168df9e064eadfc8cb65e80b7f3
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-10-20 12:21:37 +0200

    feat(PersonalInformation): Add personal information and image background

--- components/PersonalInformation/PersonalInformation.tsx
+++ components/PersonalInformation/PersonalInformation.tsx
@@ -1,45 +1,60 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {StyleSheet, Text, View, Image } from 'react-native';
 
 const PersonalInformation = () => {
   return (
-    <View>
-      <View style={styles.principalContainerInfo}>
-        <Image style={styles.avatar} source={require('../../assets/images/avatar.jpg')} />
-        <View style={styles.personalInfoContainer}>
-          <Text style={styles.title}>
-            Descripción sobre mí!
-          </Text>
-          <Text>
-            Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos
-          </Text>
+    <View style={styles.containerInfo}>
+      <Image style = {{ width: 1000, height:90, zIndex:-1}} source={require('../../../assets/images/backgroundLigth.jpg')} />
+      <View >
+        <Image style={styles.avatar} source={require('../../../assets/images/avatar.jpg')} />
+        <View style={styles.containerText}>
+          <View style={styles.text}>
+            <Text style={styles.titleText}>
+              Descripción sobre mí!
+            </Text>
+            <Text style={styles.infoText}>
+              ¡Hola! soy Claudia, estudiante de 2DAM, o "señorita Estupíñan" -.-
+            </Text>
+            <Text style={styles.infoText}>Me considero una persona responsable, o que simplemente no dejo que mi vida se descontrole, la verdad me tomo my enserio lo que hago! porque de eso vivire :D, o al menos eso espero, pero siempre lo intento hacer con una sonrisa, y alguna que otra queja por los bugs, pero y quien no ?</Text>
+          </View>
         </View>
       </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
-  principalContainerInfo: {
-    flexDirection: 'row', 
-    alignItems: 'center'
+  containerInfo: {
+    alignItems: 'center',
+  },
+  infoText: {
+    color: '#3b3b3c',
+    textAlign: 'justify',
+    marginRight: 50,
+  },
+  titleText: {
+    color: '#0e76bc',
+    fontSize: 20,
+    paddingBottom: 20
+  },
+  containerText: {
+    marginTop:'-5%',
+    marginBottom:'-5%'
+  },
+  text: {
+    color: 'black',
+    margin: 20,
+    width: '100%',
+    padding: 10,
+    
   },
   avatar: {
-    height: 90,
-    width: 90,
-    borderRadius: 100
+    marginTop:'-10%',
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    marginLeft: '70%',
+    marginBottom: '-10%',
+    zIndex: 1
   },
-  title: {
-    textAlign: 'center', 
-    fontWeight: '700', 
-    fontSize: 20
-  }, 
-  personalInfoContainer: {
-    margin: 10, 
-    backgroundColor: 'lightgray', 
-    padding: 10, 
-    borderRadius: 10, 
-    width: '70%'
-  }
 });
-
-export default PersonalInformation;
+export default PersonalInformation
